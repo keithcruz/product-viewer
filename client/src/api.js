ab20b5a94ff554caec0c1743443c53a2eed1b1e0
keithcruz <150sebastian@gmail.com>
2021-04-26 14:08:05 -0500

    use db model and controller

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,13 @@
-async function getData() {
+async function getData(limit = 100, offset = 0) {
   try {
-    const data = await fetch("/api/users");
+    const data = await fetch(`/api/users?limit=${limit}&offset=${offset}`);
     if (!data.ok) {
       throw new Error(
         `Fetch failed with error:${data.statusText} ${data.status}`
       );
     }
-    const json = await data.json();
-    return json;
+    const { users } = await data.json();
+    return users;
   } catch (err) {
     console.log(err);
     throw err;
